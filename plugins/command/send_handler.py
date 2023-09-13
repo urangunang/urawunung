68f2acd4c90ac9f1221bd7c6935ec7fe9ee3c925
steambaniadam <142197785+steambaniadam@users.noreply.github.com>
2023-09-13 17:50:47 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -45,6 +45,13 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
 
         if user.status == 'talent':
             picture = config.pic_talentgirl
+        if user.status == 'owner':
+            picture = config.pic_owner
+        if user.status == 'admin':
+            if key == hastag[0]:
+                picture = config.pic_admingirl
+            elif key == hastag[1]:
+                picture = config.pic_adminboy
         if user.status == 'daddy sugar':
             picture = config.pic_daddysugar
         if user.status == 'boyfriend rent':
