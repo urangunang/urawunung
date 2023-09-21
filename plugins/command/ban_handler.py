a24b5e522367a883e2b89f0b3a3ee4ed39c90f89
kaitod-id <108166655+kaitod-id@users.noreply.github.com>
2023-09-21 10:42:09 +0700

    Update ban_handler.py

--- plugins/command/ban_handler.py
+++ plugins/command/ban_handler.py
@@ -96,6 +96,39 @@ async def unban_handler(client: Client, msg: types.Message):
         )
 
 
+async def cek_handler(client: Client, msg: types.Message):
+    if not (z := re.search(r"^[\/]cek(\s|\n)*(\d+)", msg.text)):
+        return await msg.reply_text(
+            text="<b>Cara penggunaan cek user</b>\n<code>/cek id_user</code>\n\nContoh :\n<code>/cek 121212021</code>",
+            quote=True,
+            parse_mode=enums.ParseMode.HTML
+        )
+    target = z[2]
+    db = Database(int(target))
+    if not await db.cek_user_didatabase():
+        return await msg.reply_text(
+            text=f"<i>{await get_user_mention(target, client)}</i> tidak terdaftar didatabase",
+            quote=True,
+            parse_mode=enums.ParseMode.HTML
+        )
+    user_data = db.get_data_pelanggan()
+    ban_reason = db.get_data_bot(client.id_bot).ban.get(target, "Tidak ada alasan yang tersedia.")
+    
+    # Memeriksa peran pengguna sebelum menampilkan data
+    if user_data.status not in ['member', 'admin', 'owner', 'talent']:
+        return await msg.reply_text(
+            text="<i>Anda tidak memiliki izin untuk melihat data pengguna ini.</i>",
+            quote=True,
+            parse_mode=enums.ParseMode.HTML
+        )
+    
+    return await msg.reply_text(
+        text=f"Data pengguna:\n\nID Pengguna: {user_data.id}\nUsername: {user_data.username}\nNama: {user_data.nama}\nStatus: {user_data.status}\nMenfess: {user_data.menfess}\nAlasan Banned: {ban_reason}",
+        quote=True,
+        parse_mode=enums.ParseMode.HTML
+    )
+
+
 async def get_user_mention(user_id: str, client: Client):
     user_info = await client.get_users(int(user_id))
     mention_name = user_info.first_name if not user_info.last_name else f"{user_info.first_name} {user_info.last_name}"
