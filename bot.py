b45476bcd9b3dc3b8b0666766a8d1e7fdbe2a7b0
kaitod-id <108166655+kaitod-id@users.noreply.github.com>
2023-09-21 12:21:06 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-buatlah agar ketika menambahkan bot command akan otomatis memperbarui command pada bot telegram,pada kode dibawah:
-
 import config, sys, os, requests
 
 from plugins import Database
@@ -19,6 +17,7 @@ class Bot(Client):
             },
             bot_token=config.bot_token
         )
+
     async def start(self):
         await super().start()
         bot_me = await self.get_me()
@@ -60,12 +59,13 @@ class Bot(Client):
         self.username = bot_me.username
         self.id_bot = bot_me.id
         data.append(self.id_bot)
+
         # Mendapatkan perintah bot yang sudah ada
         existing_commands = await self.get_my_commands()
         
         # Daftar perintah baru yang ingin Anda tambahkan
         new_commands = [
-            BotCommand('status', 'Cek Penipu/Bukan'
+            BotCommand('cek', '🔎 Cek Penipu/Bukan'),
             BotCommand('status', '🍃 check status'),
             BotCommand('talent', '👙 talent konten / vcs'),
             BotCommand('daddysugar', '👔 daddy sugar trusted'),
@@ -84,11 +84,11 @@ class Bot(Client):
         await self.set_my_commands(existing_commands)
 
         print('BOT TELAH AKTIF')
-    
+
     async def stop(self):
         await super().stop()
         print('BOT BERHASIL DIHENTIKAN')
-    
+
     async def kirim_pesan(self, x: str):
         db = Database(config.id_admin).get_pelanggan()
         pesan = f'<b>TOTAL USER ( {db.total_pelanggan} ) PENGGUNA 📊</b>\n'
@@ -98,3 +98,4 @@ class Bot(Client):
         a = requests.get(f'{url}/sendMessage?chat_id={config.channel_log}&text={pesan}&parse_mode=HTML').json()
         requests.post(f'{url}/pinChatMessage?chat_id={config.channel_log}&message_id={a["result"]["message_id"]}&parse_mode=HTML')
         requests.post(f'{url}/deleteMessage?chat_id={config.channel_log}&message_id={a["result"]["message_id"] + 1}&parse_mode=HTML')
+
